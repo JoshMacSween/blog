10f84edae0cb6fb4c14cfe265af07ffd55b11a10
Joshua MacSween <joshmacsween@gmail.com>
2019-06-27 09:32:32 -0600

    basic controller, rest

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,11 +1,15 @@
 class ArticlesController < ApplicationController
   def index
+    @article = Article.all
   end
 
   def new
+    @article = Article.new
   end
 
   def create
+    @article = Article.create
+    redirect_to articles_path(@article)
   end
 
   def show
--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,5 @@
 Rails.application.routes.draw do
-  get 'articles/index'
-  get 'articles/new'
-  get 'articles/create'
-  get 'articles/show'
-  get 'articles/edit'
-  get 'articles/update'
-  get 'articles/delete'
+  root 'articles#index'
+  resources :articles
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
